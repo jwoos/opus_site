b8c4bc39a18b673effcee1ff12aca88a7d2b9b82
Junwoo <Jun Woo Shin>
2015-03-08 18:36:22 -0400

    working on login and sign up dialog | added firebase element

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -6,12 +6,45 @@
   // Learn more about auto-binding templates at http://goo.gl/Dx1u2g
   var app = document.querySelector('#app');
   app.appName = 'OPUS';
-  app.user = true;
-  
+
   // Listen for template bound event to know when bindings
   // have resolved and content has been stamped to the page
   app.addEventListener('template-bound', function() {
-    console.log('Our app is ready to rock!');
+    console.log('template bound');
+  });
+
+  window.addEventListener('polymer-ready', function(e) {
+    app.login = function() {
+      this.$.baseLogin.login();
+    };
+    app.logout = function() {
+      this.$.baseLogin.logout();
+    };
+
+    app.loginDialog = function() {
+      this.$.dialog1.toggle();
+    };
+    app.signupDialog = function() {
+      this.$.dialog2.toggle();
+    };
+
+    app.clearConfirm2 = function () {
+      var confirmValue = function() {
+        confirm('Are you sure you want to leave and clear all the fields?');
+      };
+      console.log('confirmValue is' + ' ' + confirmValue);
+
+      if (confirmValue === true) {
+        var input = document.getElementsByTagName('input').value;
+        input = '';
+        console.log(input);
+        this.$.dialog2.toggle();
+      } else {
+
+      }
+    };
+    
+    console.log('Polymer is ready');
   });
 
 // wrap document so it plays nice with other libraries
